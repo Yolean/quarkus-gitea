60356c016f3e92fad143d2e40f1bedb3804e4442
Staffan Olsson <staffan@repos.se>
2024-12-06 10:30:50 +0100

    fixes the multiple @Consumes issue

--- openapi-spec-update.sh
+++ openapi-spec-update.sh
@@ -6,5 +6,7 @@ set -eo pipefail
 mkdir -p runtime/src/main/openapi
 curl -LS https://docs.gitea.com/redocusaurus/plugin-redoc-1.yaml \
   | sed 's/{{AppSubUrl | JSEscape}}/http:\/\/localhost/' \
-  | yq -o yaml -P -I2 '.' \
+  | yq -o yaml -P -I2 '
+    .consumes = [.consumes[0]]
+  ' \
   > runtime/src/main/openapi/gitea.yaml
